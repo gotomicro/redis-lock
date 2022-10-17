e791c57ded206abc5d70b566fe61c7b33a96e380
Deng Ming <mingflycash@gmail.com>
2022-10-17 18:48:01 +0800

    e2e refactor

--- lock_e2e_test.go
+++ lock_e2e_test.go
@@ -21,19 +21,82 @@ import (
 	"github.com/go-redis/redis/v9"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 	"testing"
 	"time"
 )
 
-func TestClient_TryLock_e2e(t *testing.T) {
-	rdb := redis.NewClient(&redis.Options{
+type ClientE2ESuite struct {
+	suite.Suite
+	rdb redis.Cmdable
+}
+
+func (s *ClientE2ESuite) SetupSuite() {
+	s.rdb = redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
-	client := NewClient(rdb)
 	// 确保测试的目标 Redis 已经启动成功了
-	client.Wait()
+	for s.rdb.Ping(context.Background()) != nil {
+
+	}
+}
+
+func TestClientE2E(t *testing.T) {
+	suite.Run(t, &ClientE2ESuite{})
+}
+
+func (s *ClientE2ESuite) TestLock() {
+	t := s.T()
+	rdb := s.rdb
+	client := NewClient(rdb)
+	testCases := []struct {
+		name string
+
+		key        string
+		expiration time.Duration
+
+		wantLock *Lock
+		wantErr  error
+
+		before func()
+		after  func()
+	}{
+		{
+			// 一次加锁成功
+			name:       "locked",
+			key:        "locked-key",
+			expiration: time.Minute,
+			before:     func() {},
+			after: func() {
+				res, err := rdb.Del(context.Background(), "locked-key").Result()
+				require.NoError(t, err)
+				require.Equal(t, int64(1), res)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.before()
+			l, err := client.TryLock(context.Background(), tc.key, tc.expiration)
+			assert.Equal(t, tc.wantErr, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.key, l.key)
+			assert.Equal(t, tc.expiration, l.expiration)
+			assert.NotEmpty(t, l.value)
+			tc.after()
+		})
+	}
+}
+
+func (s *ClientE2ESuite) TestTryLock() {
+	t := s.T()
+	rdb := s.rdb
+	client := NewClient(rdb)
 	testCases := []struct {
 		name string
 
@@ -98,15 +161,9 @@ func TestClient_TryLock_e2e(t *testing.T) {
 	}
 }
 
-func TestLock_Unlock_e2e(t *testing.T) {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
+func (s *ClientE2ESuite) TestUnLock(t *testing.T) {
+	rdb := s.rdb
 	client := NewClient(rdb)
-	// 确保测试的目标 Redis 已经启动成功了
-	client.Wait()
 	testCases := []struct {
 		name string
 
@@ -144,11 +201,5 @@ func TestLock_Unlock_e2e(t *testing.T) {
 			err := tc.lock.Unlock(context.Background())
 			require.Equal(t, tc.wantErr, err)
 		})
-	}
-}
-
-func (c *Client) Wait() {
-	for c.client.Ping(context.Background()).Err() != nil {
-
 	}
 }
--- script/lua/lock.lua
+++ script/lua/lock.lua
@@ -1,8 +1,7 @@
-if redis.call("get", KEYS[1]) == ARGV[1]
-then
+if redis.call("get", KEYS[1]) == ARGV[1] then
     -- 刷新过期时间
-    return redis.call("pexpire", KEYS[1], ARGV[2])
+    return redis.call("pexpire", KEYS[1], ARGV[2]) == 1
 else
     -- 设置 key value 和过期时间
-    return redis.call("set", KEYS[1], ARGV[1], "NX", "PX", ARGV[2])
+    return redis.call("set", KEYS[1], ARGV[1], "NX", "PX", ARGV[2]) == "OK"
 end
