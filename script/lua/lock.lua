67810abcd92fe42f407f6430ee3c7121b177b064
zhpengsong <307238518@qq.com>
2022-11-14 17:44:50 +0800

    lua设置过期时间统一成秒级时间戳

--- script/lua/lock.lua
+++ script/lua/lock.lua
@@ -2,7 +2,7 @@ local val = redis.call('get', KEYS[1])
 -- 在加锁的重试的时候，要判断自己上一次是不是加锁成功了
 if val == false then
     -- key 不存在
-    return redis.call('set', KEYS[1], ARGV[1], 'PX', ARGV[2])
+    return redis.call('set', KEYS[1], ARGV[1], 'EX', ARGV[2])
 elseif val == ARGV[1] then
     -- 刷新过期时间
     redis.call('expire', KEYS[1], ARGV[2])
